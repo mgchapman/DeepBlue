e4fd4af2d3c26be256a2e6619c8632a6ce1d27d7
Matthew Chapman <mattchapman@live.co.uk>
2019-12-20 22:40:10 +0000

    Working version of scraper

--- src/lichessScraper.js
+++ src/lichessScraper.js
@@ -4,14 +4,14 @@ const $ = require('cheerio');
 //lichessScraper takes a url of the form https://lichess.org/@/[username]/perf/[variant] and returns the peak rating
 
 function LichessScraper(url) {
-    rp(url)
+    return rp(url)
      .then(function(html){
-       return {
-       peakRating : $('h2 > strong', html).children().first().text(),
-       };
+        peakRating = $('h2 > strong', html).children().first().text();
+	return peakRating;
       })
      .catch(function(err){
        //handle error
+       console.log(err);
      })
  };
 
